b5650b3c4eeaf044c07882db3e6a2c3be4a117cd
master8848 <jobarsanjel2@gmail.com>
2022-11-18 08:08:54 +0545

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { Button, FlatList, Text, TextInput, View } from "react-native";
+import { StyledComponent } from "nativewind";
+import { useState } from "react";
+type textType = { key: string; value: string }[];
+export default function MyComponent() {
+  const [Test, setTest] = useState<textType>([]);
+  const [text, setText] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StyledComponent component={View} className=" py-10  h-full  bg-black ">
+      <StyledComponent
+        component={TextInput}
+        tw="p-2 bg-blue-700 rounded"
+        value={text}
+        onChangeText={(text) => {
+          setText(text);
+        }}
+      ></StyledComponent>
+      <StyledComponent
+        component={Button}
+        title="Submit"
+        onPress={() =>
+          setTest((c) => [...c, { value: text, key: Math.random().toString() }])
+        }
+      />
+      <StyledComponent
+        component={FlatList}
+        className=" mt-12 p-3 "
+        data={Test}
+        renderItem={(item) => (
+          <StyledComponent
+            component={Text}
+            tw="text-white my-1 bg-blue-400 p-2"
+            onPress={() =>
+              setTest((c) => c.filter((d) => d.key != item.item.key))
+            }
+          >
+            {item.item.value}
+          </StyledComponent>
+        )}
+      />
+    </StyledComponent>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
